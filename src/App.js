708c03162fe7ffa65633ea54bcf80727a622596c
Prathyusha1993 <pkr131023@gmail.com>
2020-09-11 11:43:40 -0700

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import Movie from './components/movie';
-//import List from './components/list.js';
 
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
